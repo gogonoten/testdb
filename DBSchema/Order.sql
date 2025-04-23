cc6fbab6e7f3ee5d05db44057d1b711b275db0e3
Jacob Madsen <madsen.jacob92@gmail.com>
2025-04-23 09:56:37 +0200

    Added Order.sql

---
+++ DBSchema/Order.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `order` (
+    id INT PRIMARY KEY,
+    customer_id INT,
+    payment ENUM('MobilePay', 'Creditcard', 'Cash'),     
+    shipping ENUM('Postnord', 'GLS', 'Pickup'),     
+    product_id INT
+);
+
