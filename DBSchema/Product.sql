9ee53c897a11ee188e2776fe0cd89df06fb18b9a
Tetiana-lab <tania.reshetylova@gmail.com>
2025-04-23 13:53:11 +0200

    Update Product.sql

--- DBSchema/Product.sql
+++ DBSchema/Product.sql
@@ -1,11 +1,11 @@
 CREATE TABLE product (
- INT AUTO_INCREMENT PRIMARY KEY,
+ id INT PRIMARY KEY,
   alcohol_id INT  NOT NULL,
-  product_type  TEXT NOT NULL,
-  product_name  TEXT  NOT NULL,
+  product_type TEXT NOT NULL,
+  product_name TEXT  NOT NULL,
   percent DECIMAL(2,2),
-  nationality   TEXT,
+  nationality TEXT,
   age INT,
   price DECIMAL(5,2),
-  customer_id INT   NOT NULL,
+  customer_id INT NOT NULL
 );
